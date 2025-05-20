de87dd92f4c18ec6f0f29fd6592e6ce041468bb9
PhilemonVanDussen <vandussenph709@northwested.org>
2025-05-20 14:28:43 -0400

    update 1

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 # Pygame game template
-
-import pygame
+import pygame as pg
 import sys
-import config # Import the config module
+import config as c # Import the config module
+from enemy import Enemy
 
 def init_game():
-    pygame.init()
-    pygame.font.init
-    screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constants from config
-    pygame.display.set_caption(config.TITLE)
+    pg.init()
+    pg.font.init
+    screen = pg.display.set_mode((c.WINDOW_WIDTH, c.WINDOW_HEIGHT)) # Use constants from config
+    pg.display.set_caption(c.TITLE)
     return screen
 
 def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             return False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
                 return False
     return True
 
 
 
 
-
-
 def main():
     screen = init_game()
-    clock = pygame.time.Clock() # Initalize the clock here
-
-
+    clock = pg.time.Clock() # Initalize the clock here
+    # Enenmy
+    enemy_group = pg.sprite.Group()
+    enemy_image = pg.image.load('images/enemy_1.png').convert_alpha()
+    enemy = Enemy((200, 300), enemy_image)
+    enemy_group.add(enemy)
 
 
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.GREEN) # Use color from config
+        screen.fill(c.GREEN) # Use color from config
         
+        enemy.move()
+        enemy_group.draw(screen)
 
 
 
-
-        pygame.display.flip()
+        pg.display.flip()
         # Limit the frame rate to the specified frames per second
-        clock.tick(config.FPS) # Use the clock to control the frame rate
+        clock.tick(c.FPS) # Use the clock to control the frame rate
 
-    pygame.quit()
+    pg.quit()
     sys.exit()
 
 if __name__ == '__main__':
